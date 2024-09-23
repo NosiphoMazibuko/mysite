12a4052d1b63f8f53844db277536cd356636f22f
Nosipho Mazibuko <mazibukonossy1@gmail.com>
2024-09-23 13:58:58 +0200

    Add docstrings to admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,9 @@
+"""
+This module registers the BandMember model with the Django admin interface,
+allowing the BandMember model to be managed via the Django admin site.
+"""
 from django.contrib import admin
 from .models import BandMember
 
+# Register the BandMember model to make it visible in the Django admin site
 admin.site.register(BandMember)
